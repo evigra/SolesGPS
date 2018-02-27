97a66229a65028ba51dfb47290ec9b22e60eace5
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-02-27 17:48:25 -0600

    Automatico

--- modulos/devices_geofences/modelo.php
+++ modulos/devices_geofences/modelo.php
@@ -316,7 +316,7 @@
 						"input"							=>"false",
 						"template_title"				=>"false",
 						"height"						=>"false",
-						"header"						=>"20px",
+						"header"						=>"1px",
 						"where"	=> array(
 							"geofenceid='$geofenceid'"
 						)

3c7f0cb4b48ad65ab3df07c9401d8d7273971eaa
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-02-27 19:18:11 -0600

    Automatico

--- modulos/devices_geofences/modelo.php
+++ modulos/devices_geofences/modelo.php
@@ -208,15 +208,17 @@
 			if($option=="")									$option=array();			
 			if(!isset($option["where"]))					$option["where"]=array();
 			
-			if(!isset($option["select"]))					$option["select"]=array();
-			
-			$option["select"]["id"]							="id";			
-			$option["select"]["SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(time_end,time))))"]	="diferencia";			
-			$option["select"]["count(time)"]				="time";
-			$option["select"]["left(time_end,10)"]			="time_end";						
-			$option["select"]["deviceid"]					="deviceid";
-			$option["select"]["geofenceid"]					="geofenceid";						
-			
+			if(!isset($option["select"]))					
+			{
+				$option["select"]=array();
+				
+				$option["select"]["id"]							="id";			
+				$option["select"]["SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(time_end,time))))"]	="diferencia";			
+				$option["select"]["count(time)"]				="time";
+				$option["select"]["left(time_end,10)"]			="time_end";						
+				$option["select"]["deviceid"]					="deviceid";
+				$option["select"]["geofenceid"]					="geofenceid";						
+			}			
 			#$option["select"]="devices_geofences.geofenceid, devices_geofences.deviceid, count(devices_geofences.time) as time, count(devices_geofences.time_end) as time_end";
 			
 			#$option["where"][]							="time BETWEEN SUBDATE(left(DATE_SUB(now(),INTERVAL {$_SESSION["user"]["huso_h"]} HOUR),10),WEEKDAY(left(DATE_SUB(now(),INTERVAL {$_SESSION["user"]["huso_h"]} HOUR),10))) AND ADDDATE(left(DATE_SUB(now(),INTERVAL {$_SESSION["user"]["huso_h"]} HOUR),10),WEEKDAY(left(DATE_SUB(now(),INTERVAL {$_SESSION["user"]["huso_h"]} HOUR),10))+1)";			
@@ -317,10 +319,19 @@
 						"template_title"				=>"false",
 						"height"						=>"false",
 						"header"						=>"1px",						
+						"select"=>array(
+							"id"=>"id",
+							"SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(time_end,time))))"=>"diferencia",
+							"count(time)"				=>"time",
+							"left(time_end,10)"			=>"time_end",
+							"geofenceid"				=>"geofenceid",
+						),
 						"where"	=> array(
 							"geofenceid='$geofenceid'"
-						)
+						),
+						"group"							=>"geofenceid",
 					);
+					
 					$recinto=$this->__REPORT_SEMANA_TOTAL($option_detalle);
 					$reportes["html"].="<br><br>".$recinto["html"];
 
@@ -330,9 +341,17 @@
 						"template_title"				=>"false",
 						"height"						=>"false",
 						"header"						=>"1px",						
+						"select"=>array(
+							"id"=>"id",
+							"SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(time_end,time))))"=>"diferencia",
+							"count(time)"				=>"time",
+							"left(time_end,10)"			=>"time_end",
+							"geofenceid"				=>"geofenceid",
+							"deviceid"					=>"deviceid",
+						),						
 						"where"	=> array(
 							"geofenceid='$geofenceid'"
-						),
+						),						
 						"group"							=>"deviceid, geofenceid",						
 						
 					);

af0c10eb5b824a8c5b76b3ca6d4f1cf29ef12cf2
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-02-27 22:09:46 -0600

    Automatico

--- modulos/devices_geofences/modelo.php
+++ modulos/devices_geofences/modelo.php
@@ -312,11 +312,11 @@
 				WHERE	1=1 
 					and time BETWEEN '2018-02-19 00:00:00' AND '2018-02-25 23:59:59'
 				ORDER BY geofenceid asc, deviceid asc, time desc
-				LIMIT 10;    
+				LIMIT 20;    
     		";
     		$datas 	            = $this->__EXECUTE($comando_sql);    		
 			
-			#$this->__PRINT_R($datas);
+			$this->__PRINT_R($datas);
 			$data=array();
 			foreach($datas as $rows)
 			{				

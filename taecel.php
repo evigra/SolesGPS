c0fbb480afeedb3acd55d9914a301af3249dd97c
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-03-06 18:42:15 -0600

    Automatico

--- taecel.php
+++ taecel.php
@@ -4,15 +4,13 @@
 			#$postvars = array( 'key'=>'25d55ad283aa400af464c76d713c07ad', 'nip'=>'25d55ad283aa400af464');
 			#$url = "https://taecel.com/app/api/getProducts";
 			
-			$postvars = array( 'key'=>'25d55ad283aa400af464c76d713c07ad', 'nip'=>'25d55ad283aa400af464','producto'=>'TEL050','referencia'=>'3121204804');
+			$postvars = array( 'key'=>'25d55ad283aa400af464c76d713c07ad', 'nip'=>'25d55ad283aa400af464','producto'=>'MOV100','referencia'=>'5555555560');
 			$url = "https://taecel.com/app/api/RequestTXN";
 			
 			curl_setopt($ch,CURLOPT_URL,$url);
 			curl_setopt($ch,CURLOPT_POST, 1);                //0 for a get request
 			curl_setopt($ch,CURLOPT_POSTFIELDS,$postvars);
 			curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
-			curl_setopt($ch,CURLOPT_TIMEOUT, 20);
 			$response = curl_exec($ch);
 	
 			$response=json_decode($response);
@@ -27,8 +25,7 @@
 			curl_setopt($ch,CURLOPT_POST, 1);                //0 for a get request
 			curl_setopt($ch,CURLOPT_POSTFIELDS,$postvars);
 			curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch,CURLOPT_CONNECTTIMEOUT ,3);
-			curl_setopt($ch,CURLOPT_TIMEOUT, 20);
+
 			$response = curl_exec($ch);
 	
 			$response=json_decode($response);

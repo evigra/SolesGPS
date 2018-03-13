525a240b05d71c7524ade13ff8d70be44b186e70
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-03-13 11:18:08 -0600

    Automatico

--- modulos/movimiento/modelo.php
+++ modulos/movimiento/modelo.php
@@ -145,6 +145,9 @@
 			
 			if($this->request["sys_action_movimiento"]=="__SAVE_pagar")
 			{
+				$this->__PRINT_R($this->request);
+				
+				
 				$datas["movimiento_id"]	=$this->sys_primary_id;
 				$datas["tipo"]			="PAG";
 				$this->sys_primary_id	="";

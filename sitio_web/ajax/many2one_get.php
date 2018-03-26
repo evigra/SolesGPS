59192a2f43c3038b670d2b732246c79f233980a9
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-03-26 12:02:56 -0600

    Automatico

--- sitio_web/ajax/many2one_get.php
+++ sitio_web/ajax/many2one_get.php
@@ -43,7 +43,7 @@
 		if(@$obj_class->sys_fields[$field]["type"]=="autocomplete")
 		{
 			$obj_class->sys_fields[$field]["value"]=$value;
-			#$obj_class->__PRINT_R($obj_class->sys_fields[$field]);	
+			$obj_class->__PRINT_R($obj_class->sys_fields[$field]);	
 			$json=$obj_class->__JSON_AUTOCOMPLETE($obj_class->sys_fields[$field]);
 			$obj_class->__PRINT_R($json);
 		}

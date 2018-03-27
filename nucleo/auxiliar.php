1936cf422490ecc7d9e544736c89393cac923752
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-03-27 11:35:59 -0600

    Automatico

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -177,7 +177,7 @@
 										
 										$eval="
 											$"."option=array();
-											$"."option[\"echo\"]=array(\"CLASS {$value["class_name"]}\");
+											#$"."option[\"echo\"]=array(\"CLASS {$value["class_name"]}\");
 											$"."option[\"where\"]=array(\"{$value["class_field_m"]}='{$datas[0][$value["class_field_o"]]}'\");
 											$"."$field=$"."this->$field"."_obj->__BROWSE($"."option);
 											$"."this->sys_fields[\"$field\"][\"values\"]=\"\";

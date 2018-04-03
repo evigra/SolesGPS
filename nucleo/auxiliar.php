8cf5a2769c96d355122b248b6f18714c849ed847
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-04-02 22:41:31 -0500

    Automatico

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -1033,10 +1033,6 @@
 											changeYear: 	false,
 											currentText: 	\"Ahora\",
 											closeText: 		\"Listo\",
-											showSecond: 	false,			
-											showMillisec:	false,
-											showMicrosec:	false,
-											showTimezone:	false,											
 											dayNamesMin: [\"Do\", \"Lu\", \"Ma\", \"Mi\", \"Ju\", \"Vi\", \"Sa\"],
 											monthNames: [\"Enero\", \"Febrero\", \"Marzo\", \"Abril\", \"Mayo\", \"Junio\", \"Julio\", \"Agosto\", \"Septiembre\", \"Octubre\", \"Noviembre\", \"Diciembre\"],
 											monthNamesShort: [\"Ene\", \"Feb\", \"Mar\", \"Abr\", \"May\", \"Jun\", \"Jul\", \"Ago\", \"Sep\", \"Oct\", \"Nov\", \"Dic\"]

de08fddc58b574c0994486f50b903df7817c0cda
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-06-03 20:52:05 -0500

    Automatico

--- modulos/tareas/modelo.php
+++ modulos/tareas/modelo.php
@@ -136,7 +136,7 @@
 				$cronshistory_data["resume"]	="<br>Desde la tarea:<br>".$navegador;
 				$cronshistory_data["date"]		=@$this->sys_date;
 				#$cronshistory_data["cron_id"]	=@$row["id"];								
-				$cronshistory_data["codigo"]	=@$row["name"];								
+				$cronshistory_data["codigo"]	=@$row["class"];								
 
 
 				#echo "<br><br><br>$eval";

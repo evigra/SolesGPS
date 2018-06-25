57e2b06535674896a8791f55665d61ba9025f0bf
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-06-25 00:34:49 -0500

    Automatico

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -1875,8 +1875,7 @@
 				
 				#/*	
 				$view_title_data	=$this->__VIEW_TEMPLATE_TITLE($option);		
-				
-					
+
 
 				$view_title			=$view_title_data["view_title"];
 				$view_title_pdf		=$view_title_data["view_title_pdf"];
@@ -2075,7 +2074,7 @@
 										
 					$return["title"]=$view_title;
 
-					if(!isset($option["height"]))					$option["height"]="99%";
+					if(!isset($option["height"]))					$option["height"]="100%";
 					
 					$height_render="height:{$option["height"]};";
 					$min_height		="min-height: 140px;";
@@ -2131,22 +2130,33 @@
 							}						
 						";
 						
-					}				
-					
+					}		
+#							
+#<div id=\"div_$name\" class=\"render_h_destino\" style=\"width:100%; overflow-y:auto; overflow-x:hidden; $min_height  padding:0px; margin:0px;\">					
 					if(!in_array(@$this->request["sys_action"],$this->sys_print))					
 						$return["report"]="
-							$view_head
-							<div id=\"div_$name\" class=\"render_h_destino\" style=\"width:100%; overflow-y:auto; overflow-x:hidden; $min_height  padding:0px; margin:0px;\">
+							$view_head							
+							
+							<div id=\"div_$name\" class=\"report_class\" obj=\"$name\" style=\"height: 100%;\">
+							<div id=\"div2_$name\" style=\"width:100%; overflow-y:auto; overflow-x:hidden; padding:0px; margin:0px;\">
+
+
+
+
+
 						";
-					
-					$return["report"].="						
-							<table width=\"100%\" style=\"background-color:#fff; color:#000;  padding:0px; margin:0px;\">
+/*
 							$view_title
 							$view_body
+*/					
+					$return["report"].="						
+							<table width=\"100%\" style=\"background-color:#fff; color:#000;  padding:0px; margin:0px;\">
+								$view_body
 							</table>
 					";
 					if(!in_array(@$this->request["sys_action"],$this->sys_print))
 						$return["report"].="							
+							</div>
 							</div>		
 						";						
 					if(!in_array(@$this->request["sys_action"],$this->sys_print))
@@ -2209,10 +2219,10 @@
 						</script>
 					";
 					
-					
+					#<div id=\"base_$name\" class=\"render_h_origen\" diferencia_h=\"-40\" style=\"$height_render width:100%; overflow-y:auto; overflow-x:hidden; border: 	1px solid #ccc; padding:0px; margin:0px;\">
 					if(!in_array(@$this->request["sys_action"],$this->sys_print))					
 						$view="
-						<div id=\"base_$name\" class=\"render_h_origen\" diferencia_h=\"-40\" style=\"$height_render width:100%; overflow-y:auto; overflow-x:hidden; border: 	1px solid #ccc; padding:0px; margin:0px;\">
+						<div id=\"base_$name\" class=\"render_h_origen\" diferencia_h=\"-20\" style=\"$height_render width:100%; overflow-y:auto; overflow-x:hidden; border: 	1px solid #ccc; padding:0px; margin:0px;\">
 					";		
 
 					$view.="{$return["report"]}";

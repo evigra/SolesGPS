cc93545f1883ee049dd2eee0f5c4640983c33874
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-06-30 19:43:41 -0500

    Automatico

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -2138,17 +2138,17 @@
 							$view_head														
 							<div id=\"div_$name\" class=\"report_class\" obj=\"$name\" style=\"height:calc(100% - 120px); overflow-y:auto; padding:0px; margin:0px;\">
 								<div id=\"div2_$name\" style=\"width:100%; overflow-x:hidden;\">
-								$view_body
 						";
-/*
-							$view_title
-							
-					
 					$return["report"].="						
 							<table width=\"100%\" style=\"background-color:#fff; color:#000;  padding:0px; margin:0px;\">
 								$view_body
 							</table>
 					";
+						
+/*
+							$view_title
+							$view_body
+					
 */					
 					if(!in_array(@$this->request["sys_action"],$this->sys_print))
 						$return["report"].="							

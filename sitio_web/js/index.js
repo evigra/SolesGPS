dcb9b933258957ac66752f4e96420c523992b08b
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-07-01 22:39:49 -0500

    Automatico

--- sitio_web/js/index.js
+++ sitio_web/js/index.js
@@ -623,27 +623,27 @@
  		if($("td#module_body").length>0 && $("div#module_body").length>0) 
  		{
  			//alert(1);
-	 		//render($("td#module_body"), $("div#module_body"),0);
+	 		render($("td#module_body"), $("div#module_body"),0);
 		}
  		if($("div.report_class").length>0) 
  		{
  			var obj	=$("div.report_class").attr("obj"); 		
 	 		{
 	 			//alert(2);
-	 			//render($("div#div_"+obj), $("div#div2_"+obj),-38);		 		
+	 			render($("div#div_"+obj), $("div#div2_"+obj),-38);		 		
 	 		}
 			var alto  =$("div#div_"+obj).height() -38;
 			//if(alto>60)
 			{
 				//alert(3);
-				//$("div#div_"+obj).height(alto);	   
+				$("div#div_"+obj).height(alto);	   
 			}	
 		}
 	 	else
 	 	{
 	 		//if(alto>60)
 	 			//alert(4);
-		 		//render($("td#module_body"), $("div#module_body"),-20);
+		 		render($("td#module_body"), $("div#module_body"),-20);
 		 		
 	 	}
 

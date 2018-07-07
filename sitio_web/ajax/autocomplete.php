61afdf2d94c63c457c73a749fe46bfd48f9e8a57
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-07-07 18:40:38 -0500

    Automatico

--- sitio_web/ajax/autocomplete.php
+++ sitio_web/ajax/autocomplete.php
@@ -1,23 +1,17 @@
 <?php
-	
 	require_once("../../nucleo/sesion.php");
-	#require_once("../../../nucleo/general.php");
 	$datas=array();
 	$eval="
 		$"."objeto		=new {$_REQUEST["class_name"]}();		
 		$"."datas		=$"."objeto->{$_REQUEST["procedure"]}();
-	";	
-	
-	echo $eval;
+	";		
 	eval($eval);
-	
 	
 	foreach($datas as $index => $data)
 	{
 		$datas[$index]["label"]			=$data[$_REQUEST["class_field_l"]];
 		$datas[$index]["clave"]			=$data[$_REQUEST["class_field_m"]];
 	}
-
 	$datas[]=array(
 		"label"		=>"Crear registro",
 		"clave"		=>""

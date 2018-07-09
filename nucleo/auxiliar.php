f3f20abe8be39809bad864e22810069524c18616
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-07-08 22:37:43 -0500

    Automatico

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -1222,10 +1222,10 @@
 														{				
 															cache:		false,				
 															type: 		\"GET\",  				
-															source:		\"../sitio_web/ajax/autocomplete.php?class_name={$valor["class_name"]}&class_field_l={$valor["class_field_l"]}&class_field_m={$valor["class_field_m"]}$vars\",
+															source:		\"http://developer.solesgps.com/sitio_web/ajax/autocomplete.php?class_name=users&class_field_l=name&class_field_m=id\",
 															success:  function(res)
 															{	
-																
+																$(\"div#auto_$campo\").html(res);		
 															},		
 														});	
 													

24673f9151f2acbe5bd31d719250479dadda66b0
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-07-09 22:44:45 -0500

    Automatico

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -1206,7 +1206,7 @@
 							    $words["$campo"]  ="
 							    	<input id=\"auto_$campo\" $style type=\"text\"  name=\"auto_$campo\"  $attr value=\"$label\" class=\"formulario {$this->sys_name} $class\"><br>$titulo
 							    	<input id=\"$campo\" name=\"$campo\" value=\"{$valor["value"]}\"  class=\"formulario {$this->sys_name}\" type=\"hidden\">
-							    	<div id=\"auto_$campo\" title=\"Crear Registro\"></div>
+							    	<div id=\"auto_$campo\" title=\"Crear Registro\">aaaa</div>
 							    	<script>
 														var url=\"http://developer.solesgps.com/sitio_web/ajax/autocomplete.php?class_name={$valor["class_name"]}&class_field_l={$valor["class_field_l"]}&class_field_m={$valor["class_field_m"]}$vars&date=".date("YmdHis")."\";
 														
@@ -1222,7 +1222,6 @@
 															},		
 														});						
 
-										$(\"div#auto_$campo\").hide();
 										$(\"input#auto_$campo".".{$this->sys_name}\").autocomplete(
 										{		
 											source:		\"../sitio_web/ajax/autocomplete.php?class_name={$valor["class_name"]}&procedure={$valor["procedure"]}&class_field_l={$valor["class_field_l"]}&class_field_m={$valor["class_field_m"]}$vars&date=".date("YmdHis")."\",

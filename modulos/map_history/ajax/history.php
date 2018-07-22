a3f12d574af3a107f517529d85a577e130727399
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-07-22 15:49:36 -0500

    Automatico

--- modulos/map_history/ajax/history.php
+++ modulos/map_history/ajax/history.php
@@ -1,8 +1,8 @@
 <?php
     require_once("../../../nucleo/sesion.php");
-    require_once("../../../nucleo/general.php");
-	require_once("../../../modulos/position/modelo.php");
-	require_once("../modelo.php");
+#    require_once("../../../nucleo/general.php");
+#	require_once("../../../modulos/position/modelo.php");
+#	require_once("../modelo.php");
 		
 	$objeto				=new map_history();
 		

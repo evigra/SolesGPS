ccfd4da18a019325f878d594bafd87d0b08b3b1e
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-08-15 19:28:36 -0500

    Automatico

--- modulos/movimiento/modelo.php
+++ modulos/movimiento/modelo.php
@@ -31,7 +31,7 @@
 			    "value"             => "",			    
 			    "procedure"       	=> "autocomplete_empresa",
 			    "relation"          => "one2many",			    
-			    "class_name"       	=> "empresa",
+			    "class_name"       	=> "company",
 			    "class_field_l"    	=> "nombre",				# Label
 			    "class_field_o"    	=> "empresa_id",
 			    "class_field_m"    	=> "id",			    

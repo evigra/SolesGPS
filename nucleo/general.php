92a7eb4b5c5b42f8b9f2586c5aa7d988ca9a39a6
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-08-17 21:41:14 -0500

    Automatico

--- nucleo/general.php
+++ nucleo/general.php
@@ -602,9 +602,9 @@
 								$"."class_field_m										=$"."valor_campo[\"class_field_m\"];	
 								foreach($"."valores as $"."valor)
 								{	
-									if(!(isset($"."valor_campo[$"."class_field_m]) AND @$"."valor_campo[$"."class_field_m]==\"\"))
-										$"."aaaaa=1;
-									###	$"."valor[$"."class_field_m]						=$"."this->sys_primary_id;								
+									##if(!(isset($"."valor_campo[$"."class_field_m]) AND @$"."valor_campo[$"."class_field_m]==\"\"))
+									if(!(isset($"."valor_campo[$"."class_field_m]))
+									 	$"."valor[$"."class_field_m]						=$"."this->sys_primary_id;								
 
 									$"."primary_field					=@$"."this->$campo"."_obj->sys_primary_field;
 									

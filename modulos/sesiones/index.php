be06fffdd99a60d200ee5a3de30b9dc27fa45061
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-08-26 20:22:03 -0500

    Automatico

--- modulos/sesiones/index.php
+++ modulos/sesiones/index.php
@@ -1,47 +1,49 @@
 <?php
-	#require_once("modelo.php");
-
-	$objeto	=new sesiones();
+	$objeto							=	new sesiones();
+	$objeto->__SESSION();
 	#$objeto->__PRINT_R($objeto);
-
-	$objeto->words["html_head_description"]			="SolesGPS desarrollo su propia plataforma de rastreo vehicular y celular, con la finalidad de satisfacer las nececidades de sus clientes";
-	$objeto->words["html_head_keywords"] 			="GPS, RASTREO, MANZANILLO, SATELITAL, CELULAR, VEHICULAR, VEHICULO, TRACTO, LOCALIZACION, COLIMA, SOLES, SATELITE, GEOCERCAS, STREET VIEW, MAPA";
-	$objeto->words["html_head_title"]           	="SOLES GPS :: Iniciar sesion";
-
-	$objeto->words["html_head_css"]					= $objeto->__FILE_CSS(array(
-		"../".$objeto->sys_module."css/index",		
-		));	
+	
+	$objeto->words["system_body"]	=	$objeto->__TEMPLATE($objeto->sys_html."system_body");	# TEMPLATES ELEJIDOS PARA EL MODULO
+	$objeto->words["system_module"]	=	$objeto->__TEMPLATE($objeto->sys_html."system_module");
 	
 	
-	$objeto->words["html_head_js"] 					=$objeto->__FILE_JS(array("../".$objeto->sys_module."js/index"));	
-		
-
-		$objeto->words["system_body"]               =$objeto->__TEMPLATE($objeto->sys_html."system_body"); 			# TEMPLATES ELEJIDOS PARA EL MODULO
-		$objeto->words["system_module"]             =$objeto->__TEMPLATE($objeto->sys_html."system_module");	
+	$objeto->words["html_head_js"]	=	$objeto->__FILE_JS(array("../".$objeto->sys_module."js/index"));
+	#$objeto->words["html_head_css"]	=	$objeto->__FILE_CSS(array("../sitio_web/css/basicItems"));
 	
-		$option=array();
-		$option["template_title"]	                =$objeto->sys_module . "html/report_title";
-		$option["template_body"]	                =$objeto->sys_module . "html/report_body";
+
+    $module_center	=	"";
+    $module_title	=	"";
+
+    {
+
+	    $module_left	=	"";
+		$option     	=	array();
+
+		$option["template_title"]		=	$objeto->sys_module."html/report_title";
+		$option["template_body"]		=	$objeto->sys_module."html/report_body";
+		$option["template_form"]		=	$objeto->sys_module."html/report_form";
 		
-		$data										=$objeto->sesion($option);
-		$objeto->words["module_body"]				=$data["html"];
-		$module_title								="Reporte de ";
+		$data							=	$objeto->devices($option);
 		
+		$objeto->words["module_body"]	=	$data["html"];	
+		$module_title                	=	"Reporte de ";
+    	
+    }
+	
+	$objeto->words["module_title"]	=	"Sesiones Registradas";
+	$objeto->words["module_left"]  	=	$objeto->__BUTTON($module_left);
+	$objeto->words["module_center"]	=	$module_center;
+	$objeto->words["module_right"]	=	$objeto->__BUTTON($module_right);;
+		
+	#$objeto->__PRINT_R($_SESSION["user"]);
+	$objeto->words["html_head_title"]		=	"SOLES GPS :: {$_SESSION["company"]["razonSocial"]} :: {$objeto->words["module_title"]}";
+	
+	$objeto->words["html_head_description"]	=	"EN LA EMPRESA SOLESGPS, CONTAMOS CON UN MODULO PARA ADMINISTRAR EL REGISTRO DE DISPOSITIVOS GPS.";
+	$objeto->words["html_head_keywords"]	=	"GPS, RASTREO, MANZANILLO, SATELITAL, CELULAR, VEHICULAR, VEHICULO, TRACTO, LOCALIZACION, COLIMA, SOLES, SATELITE, GEOCERCAS, STREET VIEW, MAPA";
+	
+    $objeto->html                       	=	$objeto->__VIEW_TEMPLATE("system", $objeto->words);
+    $objeto->__VIEW($objeto->html);
+	#$objeto->__PRINT_R($objeto->sys_fields);
     
-    	$view	="system";
- 		
-	$objeto->html                  					= $objeto->__VIEW_TEMPLATE($view, $objeto->words);
-	$objeto->words["module_title"]					="Sesiones registradas";
-	$objeto->words["module_left"]					="";
-	$objeto->words["module_center"]					="";
-	$objeto->words["module_right"]					="";
-	
-	if(!array_key_exists("mensaje_sesion",$objeto->words))
-		$objeto->words["mensaje_sesion"]			="";
-	
-	$objeto->__VIEW($objeto->html);    
-	
-
-
+    
 ?>
-

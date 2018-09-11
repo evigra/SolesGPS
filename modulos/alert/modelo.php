3f6f8e4d0abed2dff6e0533be119f566211c4cff
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-09-11 12:06:28 -0500

    Automatico

--- modulos/alert/modelo.php
+++ modulos/alert/modelo.php
@@ -29,7 +29,7 @@
 			"descripcion"	    =>array(
 			    "title"             => "Descripcion",
 			    "showTitle"         => "si",
-			    "type"              => "txt",
+			    "type"              => "html",
 			    "default"           => "",
 			    "value"             => "",			    
 			),
@@ -98,7 +98,7 @@
     		$option["where"][]		="company_id='{$_SESSION["company"]["id"]}'";
     		$option["order"]		="fechaevento DESC";
     		
-    		$option["echo"]			="Alert";
+    		#$option["echo"]			="Alert";
     		
     		return parent::__BROWSE($option);
 		}

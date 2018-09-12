659e9afef35ebdf2d916af1930491c9c27c3fc22
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-09-11 22:04:56 -0500

    Automatico

--- modulos/movimiento/index.php
+++ modulos/movimiento/index.php
@@ -130,7 +130,7 @@
 		$option["template_body"]	                = $objeto->sys_module . "html/report_body";
 		
 				
-		$data										= $objeto->__REPORTE($option);		
+		$data										= $objeto->__VIEW_REPORT($option);		
 		$objeto->words["module_body"]				=$data["html"];
 		$module_title								="Reporte de ";
     }

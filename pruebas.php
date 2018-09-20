1fb111935e93af9731153baaad4ede9481471ef0
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-09-20 00:15:49 -0500

    Automatico

--- pruebas.php
+++ pruebas.php
@@ -1,6 +1,26 @@
 <?php
 
+$ch = curl_init();
+ 
+// definimos la URL a la que hacemos la petición
+curl_setopt($ch, CURLOPT_URL,"https://cfdiau.sat.gob.mx/nidp/wsfed/ep?id=SATUPCFDiCon&sid=0&option=credential&sid=0");
+// indicamos el tipo de petición: POST
+curl_setopt($ch, CURLOPT_POST, TRUE);
+// definimos cada uno de los parámetros
+#curl_setopt($ch, CURLOPT_POSTFIELDS, "postvar1=value1&postvar2=value2&postvar3=value3");
+ 
+// recibimos la respuesta y la guardamos en una variable
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$remote_server_output = curl_exec ($ch);
+ 
+// cerramos la sesión cURL
+curl_close ($ch);
+ 
+// hacemos lo que queramos con los datos recibidos
+// por ejemplo, los mostramos
+print_r($remote_server_output);
 
+/*
 	#date_default_timezone_set('America/Los_Angeles');
 	$script_tz = date_default_timezone_get();
 	
@@ -26,5 +46,6 @@
 		            }
 		    }
 	}
+	*/
 ?>
 

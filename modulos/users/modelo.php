9103a42f9a8158239e7033a388c75ad957b3ae1c
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-09-24 19:13:26 -0500

    Automatico

--- modulos/users/modelo.php
+++ modulos/users/modelo.php
@@ -158,7 +158,7 @@
 
     	    $user_id=parent::__SAVE($datas,$option);
     	    
-    	    #$this->__PRINT_R($this->comando_sql);
+    	    $this->__PRINT_R($this->sys_sql);
     	    ## GUARDAR PERFILES DE USUARIO
     	    $usergroup_datas=array();
     	    if(isset($datas["usergroup_ids"]))

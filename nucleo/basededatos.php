9d2c07de7106f98b0a7d344d528c6e3f08d20884
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-09-24 19:39:15 -0500

    Automatico

--- nucleo/basededatos.php
+++ nucleo/basededatos.php
@@ -109,7 +109,7 @@
     	}     	     	  
 		public function __PRINT_R($variable)
 		{  
-		    echo "<div class=\"echo\" title=\"Sistema :: {$this->sys_object}\"><pre>";
+		    echo "<div class=\"echo\" title=\"Sistema :: {$this->sys_object} {$this->sys_name}\"><pre>";
 		    @print_r(@$variable);
 		    echo "</pre></div>";			
     	} 

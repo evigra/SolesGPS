052ed9d6d0e36090d5bf3dcb213dcf1a17eb5830
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-09-25 00:06:24 -0500

    Automatico

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -1266,6 +1266,12 @@
 												source:		\"../sitio_web/ajax/autocomplete.php?class_name={$valor["class_name"]}&procedure={$valor["procedure"]}&class_field_l={$valor["class_field_l"]}&class_field_m={$valor["class_field_m"]}$vars&date=".date("YmdHis")."\",
 												dataType: 	\"jsonp\",
 												$js_auto
+												change: function( event, ui ) // CUANDO SE SELECCIONA LA OPCION REALIZA LO SIGUIENTE
+												{
+													if($(\"input#auto_$campo".".{$this->sys_name}\").val()==\"\")
+													$(\"input#$campo".".{$this->sys_name}\").val(\"\")
+																								
+												},
 												select: function( event, ui ) // CUANDO SE SELECCIONA LA OPCION REALIZA LO SIGUIENTE
 												{												
 													if(typeof auto_$campo === 'function') 								
@@ -1296,7 +1302,7 @@
 														}
 													}
 													if($(\"input#auto_$campo".".{$this->sys_name}\").val()==\"\")
-														$(\"input#$campo".".{$this->sys_name}\").val(\"\")
+													$(\"input#$campo".".{$this->sys_name}\").val(\"\")
 												}				
 											});				            	
 										</script>

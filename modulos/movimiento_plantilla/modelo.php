f1774a69c456604f8c41fe469a2e3b03d4af1945
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-09-27 19:29:13 -0500

    Automatico

--- modulos/movimiento_plantilla/modelo.php
+++ modulos/movimiento_plantilla/modelo.php
@@ -21,12 +21,14 @@
 		}
    		public function __SAVE($datas=NULL,$option=NULL)
     	{
+    		$this->__PRINT_R($this->request);
 			if($this->request["sys_section_movimiento_plantilla"]=="create")
 			{
-				$option_folios					=array();
+				$option_folios=array();
 				$option_folios["tipo"]			="PL";								
 				$datas["folio"]					=$this->__FOLIOS($option_folios);
 			}				
+			$this->__PRINT_R($datas);
     	    $return= parent::__SAVE($datas,$option);
     	    return $return;
 		}

0bd6e08b2e2c417764d36c40793cd942f3d68de8
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-10-03 18:45:48 -0500

    Automatico

--- modulos/company_gps/modelo.php
+++ modulos/company_gps/modelo.php
@@ -1,12 +1,203 @@
 <?php
-	class company_gps extends company
+	#if(file_exists("nucleo/general.php")) require_once("nucleo/general.php");
+	#require_once("modulos/files/modelo.php");
+	class company_gps extends general
 	{   
 		##############################################################################	
 		##  Propiedades	
 		##############################################################################
 		var $sys_enviroments	="DEVELOPER";
 		var $sys_table			="company";
+		var $sys_fields		=array(
+			"id"			=>array(
+			    "title"             => "id",
+			    "showTitle"         => "si",
+			    "type"              => "primary key",
+			    "default"           => "",
+			    "value"             => "",			    
+			),		
+			"razonSocial"	    	=>array(
+			    "title"             => "Compania",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",			    
+			),
+			"RFC"	    		=>array(
+			    "title"             => "RFC",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),
+			"fechaRegistro"	    	=>array(
+			    "title"             => "Fecha de Registro",
+			    "showTitle"         => "si",
+			    "type"              => "date",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),
+			"estatus"	    =>array(
+			    "title"             => "Estatus",
+			    "showTitle"         => "si",
+			    "type"              => "select",
+			    "default"           => "",
+			    "value"             => "",
+			    "source"            => array(
+			    	"1"=>"Vigente",
+			    	"0"=>"Cancelado"
+			    ),
+			),
+			"web"	    =>array(
+			    "title"             => "Web",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),
+			"sistema_web"	    =>array(
+			    "title"             => "Plataforma Web",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),
 
+			"files_id"	    =>array(
+			    "title"             => "Logo",
+			    "showTitle"         => "si",
+			    "type"              => "file",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),			
+			"img_files_id"	    =>array(
+			    "title"             => "Logo",
+			    "showTitle"         => "si",
+			    "type"              => "file",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),						
+			"img_files_id_med"	    =>array(
+			    "title"             => "Logo",
+			    "showTitle"         => "si",
+			    "type"              => "file",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),						
+
+			"Id_detalleDatos"	=>array(
+			    "title"             => "Id Detalle Datos",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),
+			"lema"	=>array(
+			    "title"             => "Lema",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),
+			"telefono"	=>array(
+			    "title"             => "Telefono",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),						
+			"extension"	    	=>array(
+			    "title"             => "Extension",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",			    
+			),				
+			
+			"mail_from"	=>array(
+			    "title"             => "Mail FROM",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),			
+			"mail_bbc"	=>array(
+			    "title"             => "Mail BBC",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),			
+			"pago_anterior"	=>array(
+			    "title"             => "Pago Anterior",
+			    "showTitle"         => "si",
+			    "type"              => "date",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),			
+			"pago_siguiente"	=>array(
+			    "title"             => "Pago Siguiente",
+			    "showTitle"         => "si",
+			    "type"              => "date",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),			
+			"chat_whatsapp"	    	=>array(
+			    "title"             => "Grupo WhatsApp",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",			    
+			),							
+			"nombre"	    	=>array(
+			    "title"             => "Razon Social",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",			    
+			),			
+
+			"email"	    	=>array(
+			    "title"             => "Email",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",			    
+			),			
+
+			"domicilio_fiscal"	=>array(
+			    "title"             => "Domicilio Fiscal",
+			    "showTitle"         => "si",
+			    "type"              => "input",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),			
+			"tipo_company"	=>array(
+			    "title"             => "TIPO",
+			    "showTitle"         => "si",
+			    "type"              => "hidden",
+			    "default"           => "",
+			    "value"             => "",
+			    "value"             => "",
+			),			
+
+
+		);				
 		##############################################################################	
 		##  Metodos	
 		##############################################################################		
@@ -21,7 +212,7 @@
     	    if(!is_null($files_id))		$datas["files_id"]			=$files_id;
     	    
     	    if(!isset($datas["tipo_company"]) OR @$datas["tipo_company"]=="")	
-    	   		$datas["tipo_company"]			="GPS";    		    		
+    	    $datas["tipo_company"]			="GPS";    		    		
 
     		parent::__SAVE($datas,$option);
 		}		
--- modulos/position/modelo.php
+++ modulos/position/modelo.php
@@ -382,8 +382,7 @@
 			$aux_descripcion="";
             foreach($position_data as $row)
             {
-				//           
-				//
+           
             	echo "<br><br><br>#### POSICION {$row["pos_id"]} :: {$row["dispo"]} ########";
             	$data_update	=array();
             	

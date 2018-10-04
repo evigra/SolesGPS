20ea29ef692769c4446121bb4b9a9f140d2cbcf4
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-10-04 17:21:18 -0500

    Automatico

--- nucleo/general.php
+++ nucleo/general.php
@@ -152,7 +152,7 @@
     			$this->__FIND_FIELD_ID();
     			$this->sys_sql			="DELETE FROM {$this->sys_table} WHERE {$this->sys_primary_field}='$option'";
     			    		
-    			return 	= $this->__EXECUTE($this->sys_sql);
+    			//return 	= $this->__EXECUTE($this->sys_sql);
     		}
     		
 

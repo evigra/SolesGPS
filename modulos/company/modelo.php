2546a940604ed738e1081ad6f66b814091aac279
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-10-13 15:25:38 -0500

    Automatico

--- modulos/company/modelo.php
+++ modulos/company/modelo.php
@@ -164,7 +164,7 @@
 			$return 				=parent::__BROWSE($option);
 			return	$return;     	
 		}				
-		public function autocomplete_empresa()		
+		public function __AUTOCOMPLETE()		
     	{	
     		$option					=array();
     		$option["where"]		=array();    		

d4bddd93afdfcdd848a6ef12bd227f608a66be73
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-10-14 13:46:01 -0500

    Automatico

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -714,7 +714,7 @@
 							$"."option"."_obj_$campo	=array(\"
 								\"name\"			=>\"$campo"."_obj\",		
 								\"memory\"			=>\"$campo\",
-								\"class_one\"		=>\"{$this->name}\",
+								\"class_one\"		=>\"{$this->sys_name}\",
 							);													
 							$"."this->obj_$campo   	=new {$valor["class_name"]}($"."option"."_obj_$campo);
 						";		

c0e63946c98ad8ac1fea3b692ec7f77b21353099
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-10-14 11:43:11 -0500

    Automatico

--- modulos/company/modelo.php
+++ modulos/company/modelo.php
@@ -14,7 +14,7 @@
 			    "title"             => "Company",
 			    "type"              => "input",
 			),
-			/*
+			
 			"locacion_id"	    =>array(
 			    "title"             => "Locacion",
 			    "description"       => "Responsable del dispositivo",
@@ -26,7 +26,8 @@
 			    "class_field_l"    	=> "nombre",				# Label
 			    "class_field_o"    	=> "responsable_id",
 			    "class_field_m"    	=> "id",			    
-			),			
+			),
+			/*			
 			"departamento_id"	    =>array(
 			    "title"             => "Departamento",
 			    "description"       => "Responsable del dispositivo",

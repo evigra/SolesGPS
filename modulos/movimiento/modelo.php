d70fd35c4231177b1ab021519dd338ae1ea4e7fc
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-10-15 19:41:57 -0500

    Automatico

--- modulos/movimiento/modelo.php
+++ modulos/movimiento/modelo.php
@@ -138,8 +138,8 @@
     	{
 			$datas["registro"]			=$this->sys_date;
 			$datas["company_id"]		=$_SESSION["company"]["id"];
-			#if(!isset($datas["trabajador_id"]))	
-			$datas["trabajador_id"]=$_SESSION["user"]["trabajador_id"];
+			if(!isset($datas["trabajador_id"])	OR $datas["trabajador_id"]=="")	
+				$datas["trabajador_id"]=$_SESSION["user"]["trabajador_id"];		
 
     	    return parent::__SAVE($datas,$option);
 		}

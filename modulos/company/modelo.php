38a89a25ec6a1d5b55f17c2f5346ae81109effb4
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-10-16 21:57:24 -0500

    Automatico

--- modulos/company/modelo.php
+++ modulos/company/modelo.php
@@ -171,7 +171,7 @@
 			if(!isset($option["select"]))   	$option["select"]	=array();
 
 			$option["select"]["admin_soles37.FN_ImgFile('../modulos/users/img/user.png',files_id,0,0)"]		="img_files_id";
-			$option["select"]["admin_soles37.FN_ImgFile('../modulos/users/img/user.png',files_id,180,100)"]	="img_files_id_med";				
+			$option["select"]["admin_soles37.FN_ImgFile('../modulos/users/img/user.png',files_id,100,100)"]	="img_files_id_med";				
 			$option["select"]["admin_soles37.FN_ImgFile('../modulos/users/img/user.png',files_id,100,60)"]	="img_files_id_chi";
 			$option["select"][]					="company.*";			
 

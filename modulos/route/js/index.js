11ee1d6267a5e83917fada5f18a7b7d238efe847
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-11-11 11:25:32 -0600

    Automatico

--- modulos/route/js/index.js
+++ modulos/route/js/index.js
@@ -57,48 +57,9 @@
 			limpiar_real();	  
 			
 		}	    
-		/*
-		$("#action").button({
-			icons: {	primary: "ui-icon-document" },
-			text: true
-		    })
-		    .click(function(){
-				var variables=getVarsUrl();
-				var str_url="";
-				for(ivariables in variables)
-				{
-					if(ivariables=="sys_action")	str_url+="&"+ivariables+"=__SAVE";
-					else							str_url+="&"+ivariables+"="+ variables[ivariables];
-				}		        
-				$("form")
-					.attr({"action":str_url})
-					.submit();		        
-		    }
-	    );		
-
-		$("#create").button({
-			icons: {	primary: "ui-icon-document" },
-			text: false
-		    })
-		    .click(function(){
-		        window.location="&sys_section=create&sys_action=";		    
-		    }
-	    );		
-
-		$("#write").button({
-			icons: {	primary: "ui-icon-pencil" },
-			text: false
-		    })
-		    .click(function(){
-		        window.location="&sys_section=write&sys_action=";		    
-		    }
-	    );		
-		action_cancel();
-	    link_report("&sys_section=report&sys_action=");
-
+		
     	if($("div#map").length>0) 
     	{
-
 			$("#map").click(function()
 			{
 				limpiar_virtual();
@@ -176,5 +137,5 @@
 				}
 			);	    
 		}
-		*/	            
+		            
     });

0a644502056ca6776005d8e9ff9036a0b2770e1e
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-11-24 23:29:07 -0600

    Automatico

--- modulos/travels/modelo.php
+++ modulos/travels/modelo.php
@@ -24,7 +24,7 @@
 			    "class_name"       	=> "carta_porte",			    
 			    "class_field_o"    	=> "id",
 			    "class_field_m"    	=> "movimiento_id",				
-			),
+			);
 
 
 			parent::__CONSTRUCT();		

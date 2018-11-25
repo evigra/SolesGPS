c569a58ccbf7155e4e8daa5ffb97bf9eb988b7c3
Eduardo Vizcaino Granados <evigra@hotmail.com>
2018-11-24 23:28:28 -0600

    Automatico

---
+++ modulos/carta_porte/modelo.php
@@ -0,0 +1,25 @@
+<?php
+	class carta_porte extends movimientos
+	{   
+		##############################################################################	
+		##  Propiedades	
+		##############################################################################
+		var $mod_menu=array();		
+		##############################################################################	
+		##  Metodos	
+		##############################################################################
+		public function __CONSTRUCT($option=array())
+		{	
+			$this->sys_fields["image"]=array(
+		
+		
+			parent::__CONSTRUCT($option);			
+			$this->words["html_head_js"]              	=$this->__FILE_JS(array("../".$this->sys_module."js/index"));			
+		}
+   		public function __SAVE($datas=NULL,$option=NULL)
+    	{
+    	    return parent::__SAVE($datas,$option);
+		}
+	}
+?>
+
--- modulos/travels/modelo.php
+++ modulos/travels/modelo.php
@@ -17,6 +17,16 @@
         
 		public function __CONSTRUCT()
 		{	
+			$this->sys_fields["movimientos_ids"]=array(
+			    "title"             => "Horario",
+			    "type"              => "form",
+			    "relation"          => "many2one",			    
+			    "class_name"       	=> "carta_porte",			    
+			    "class_field_o"    	=> "id",
+			    "class_field_m"    	=> "movimiento_id",				
+			),
+
+
 			parent::__CONSTRUCT();		
 		}
    		public function __SAVE($datas=NULL,$option=NULL)

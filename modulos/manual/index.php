a57192ba8d648397076785077106829273173aaf
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-02-20 22:50:54 -0600

    Update

--- modulos/manual/index.php
+++ modulos/manual/index.php
@@ -9,40 +9,21 @@
 	
 	$objeto->words["html_head_js"]	=	$objeto->__FILE_JS(array("../".$objeto->sys_module."js/index"));
 	
-	$module_left=array(
-        array("action"=>"Guardar"),
-        array("cancel"=>"Cancelar"),
-    );
-
-    $module_right=array(
-        array("create"=>"Crear"),
-        #array("write"=>"Modificar"),
-        array("kanban"=>"Kanban"),
-        array("report"=>"Reporte"),
-    );
-
     $module_center	=	"";
     $module_title	=	"";
 
 
-    	$module_title                	=	"Crear ";
+	$module_title                	=	"Crear ";
 
-    	$module_right=array(
-			#array("create"=>"Crear"),
-			#array("write"=>"Modificar"),
-			array("kanban"=>"Kanban"),
-			array("report"=>"Reporte"),
-	    	);
-
-    	$objeto->words["module_body"]	=	$objeto->__VIEW_CREATE($objeto->sys_module."html/create");	
-    	$objeto->words               	=	$objeto->__INPUT($objeto->words,$objeto->sys_fields);    
+	$objeto->words["module_body"]	=	$objeto->__VIEW_CREATE($objeto->sys_module."html/create");	
+	$objeto->words               	=	$objeto->__INPUT($objeto->words,$objeto->sys_fields);    
 
 
 	
-	$objeto->words["module_title"]	=	"$module_title Dispositivos";
-	$objeto->words["module_left"]  	=	$objeto->__BUTTON($module_left);
-	$objeto->words["module_center"]	=	$module_center;
-	$objeto->words["module_right"]	=	$objeto->__BUTTON($module_right);;
+	#$objeto->words["module_title"]	=	"$module_title Dispositivos";
+	#$objeto->words["module_left"]  	=	$objeto->__BUTTON($module_left);
+	#$objeto->words["module_center"]	=	$module_center;
+	#$objeto->words["module_right"]	=	$objeto->__BUTTON($module_right);;
 		
 	#$objeto->__PRINT_R($_SESSION["user"]);
 	$objeto->words["html_head_title"]		=	"SOLES GPS :: {$_SESSION["company"]["razonSocial"]} :: {$objeto->words["module_title"]}";

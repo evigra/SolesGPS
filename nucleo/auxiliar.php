c94a24f7dbba66597cf7bff619f2d49b04e357be
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-02-21 13:17:15 -0600

    Update

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -681,14 +681,17 @@
         ##############################################################################
 		public function __REQUEST_AUX($campo,$valor)
 		{  
-			#if(!isset($this->sys_fields["$campo"]["htmlentities"]))	$this->sys_fields["$campo"]["htmlentities"]=true;
-						
-			#if(in_array($this->sys_fields["$campo"]["htmlentities"],$this->sys_true))				
 		
-		
-			if(!is_array($valor)) 			
-				$valor	=htmlentities($valor);
+			#if(!is_array($valor)) 			
+			#	$valor	=htmlentities($valor);
 			
+			if(!isset($this->sys_fields["$campo"]["htmlentities"]))	
+				$this->sys_fields["$campo"]["htmlentities"]=true;
+						
+			if(!is_array($valor) AND in_array($this->sys_fields["$campo"]["htmlentities"], $this->sys_true))				
+					$valor	=htmlentities($valor);
+		
+		
 			
 			
 			

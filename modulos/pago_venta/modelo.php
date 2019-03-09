4734f275e4ddeb12edf0e24771a374f4bd5046f5
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-03-09 14:49:30 -0600

    Update

---
+++ modulos/pago_venta/modelo.php
@@ -0,0 +1,41 @@
+<?php
+	class pago_venta extends movimiento
+	{   
+		##############################################################################	
+		##  Propiedades	
+		##############################################################################
+		var $mod_menu			=array();
+		var $sys_enviroments	="DEVELOPER";
+		var $sys_table			="movimiento";
+		var $tipo_movimiento	="PV";
+		
+		var $movimiento_obj;
+		
+		##############################################################################	
+		##  Metodos	
+		##############################################################################
+        
+		public function __CONSTRUCT()
+		{	
+			parent::__CONSTRUCT();		
+		}
+   		public function __SAVE($datas=NULL,$option=NULL)
+    	{    					
+    	    $return= parent::__SAVE($datas,$option);
+    	    return $return;
+		}
+   		public function __BROWSE($option="")
+    	{			    	
+			if($option=="")	$option=array();			
+			if(!isset($option["where"]))	$option["where"]=array();
+			
+			$option["where"][]				="tipo='{$this->tipo_movimiento}'";   # PL plantilla
+			
+			if(!isset($this->request["sys_order_". $this->sys_object]) OR $this->request["sys_order_". $this->sys_object]=="")
+				$option["order"]="id desc";
+						
+			return parent::__BROWSE($option);
+		}							
+	}
+?>
+

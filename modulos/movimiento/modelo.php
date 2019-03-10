899d7ac05ca2ec1968db2aa7b01b7e0086daa26e
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-03-10 08:48:07 -0600

    Update

--- modulos/movimiento/modelo.php
+++ modulos/movimiento/modelo.php
@@ -201,6 +201,7 @@
 			$option["select"][]	="m1.*";
 			$option["select"]["SUM(m1.pago)"]	="pago";
 			$option["select"]["SUM(m1.orden)"]	="orden";
+			$option["select"]["SUM(m1.orden) - SUM(m1.orden)"]	="diferencia";
 
 			$option["from"]		="
 				(

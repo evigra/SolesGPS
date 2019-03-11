d4bf1b6ba2e99f06d9b418c80a9ec2669efa43e4
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-03-10 19:51:20 -0600

    Update

--- modulos/movimiento/modelo.php
+++ modulos/movimiento/modelo.php
@@ -38,6 +38,13 @@
 			    "class_field_o"    	=> "empresa_id",
 			    "class_field_m"    	=> "id",			    
 			),			
+			"movimiento_id"	    =>array(
+			    "type"              => "autocomplete",
+			    "relation"          => "one2many",			    
+			    "class_name"       	=> "movimiento",			    
+			    "class_field_o"    	=> "movimiento_id",
+			    "class_field_m"    	=> "id",				
+			),
 			"movimientos_ids"	    =>array(
 			    "type"              => "form",
 			    "relation"          => "many2one",			    

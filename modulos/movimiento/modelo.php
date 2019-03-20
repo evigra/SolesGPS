950f28293fc950c19354e9bfba1eea077248ba35
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-03-20 08:09:45 -0600

    Update

--- modulos/movimiento/modelo.php
+++ modulos/movimiento/modelo.php
@@ -208,7 +208,8 @@
 			$option["select"][]	="m1.*";
 			$option["select"]["
 				CASE 
-					WHEN SUM(m1.pago)>0 AND tipo='PV' THEN SUM(m1.pago)
+					WHEN SUM(m1.orden)>0 AND tipo='OV' THEN SUM(m1.orden)
+					WHEN SUM(m1.pago)>0 AND tipo='PV' THEN SUM(m1.pago
 					##WHEN SUM(m1.orden)>0 AND tipo='OV' THEN SUM(m1.orden)
 					##WHEN SUM(m1.pago)>0 AND tipo='PC' THEN SUM(m1.pago)
 				END							

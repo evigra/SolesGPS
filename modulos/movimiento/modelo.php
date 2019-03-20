953ab67dc67909492b4237d745aa406b46939223
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-03-19 23:02:51 -0600

    Update

--- modulos/movimiento/modelo.php
+++ modulos/movimiento/modelo.php
@@ -209,6 +209,7 @@
 			$option["select"]["
 				CASE 
 					WHEN SUM(m1.orden)>0 AND tipo='OV' THEN SUM(m1.orden)
+					WHEN SUM(m1.orden)>0 AND tipo='OC' THEN SUM(m1.orden)
 				END				
 			
 			"]	="orden";

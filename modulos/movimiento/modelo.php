d9f9711abe956ffbf4905c513c7067eeccd2bbba
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-03-22 11:14:31 -0600

    Update

--- modulos/movimiento/modelo.php
+++ modulos/movimiento/modelo.php
@@ -235,8 +235,7 @@
 					SELECT  
 						(CASE WHEN tipo IN (\"PV\",\"OC\") then total else 0 end) as PAGO,
 						(CASE WHEN tipo IN (\"OV\",\"PC\") then total else 0 end) as ORDEN,		
-						m.*,
-						m.empresa_id
+						m.*
 					FROM movimiento m WHERE tipo in (\"PV\", \"OV\",\"PC\", \"OC\")			
 					
 					UNION
@@ -244,8 +243,7 @@
 					SELECT  
 						(CASE WHEN tipo IN (\"PV\",\"OC\") then total else 0 end) as PAGO,
 						(CASE WHEN tipo IN (\"OV\",\"PC\") then total else 0 end) as ORDEN,		
-						vc.*,
-						vc.''
+						vc.*
 					FROM movimiento vc WHERE tipo in (\"PV\", \"OV\",\"PC\", \"OC\")			
 					GROUP BY tipo					
 				) m1

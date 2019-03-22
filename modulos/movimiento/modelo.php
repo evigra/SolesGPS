b1b6af9f326d235452e967a63992331ce2d981c8
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-03-21 20:38:23 -0600

    Update

--- modulos/movimiento/modelo.php
+++ modulos/movimiento/modelo.php
@@ -215,7 +215,7 @@
 					WHEN venta=1 AND SUM(m1.orden)-SUM(m1.pago)>0 THEN SUM(m1.orden)-SUM(m1.pago)
 					WHEN venta=1 AND SUM(m1.pago)-SUM(m1.orden)=0 THEN ''
 
-					WHEN compra=1 AND SUM(m1.pago)-SUM(m1.orden)>0 THEN SUM(m1.orden)-SUM(m1.pago)
+					WHEN compra=1 AND SUM(m1.orden)-SUM(m1.pago)>0 THEN SUM(m1.pago)-SUM(m1.orden)
 					#WHEN compra=1 AND SUM(m1.orden)-SUM(m1.pago)>0 THEN SUM(m1.orden)-SUM(m1.pago)
 					#WHEN compra=1 AND SUM(m1.pago)-SUM(m1.orden)=0 THEN ''
 

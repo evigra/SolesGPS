2c22eabae899df68f9937096e7519b0b312ce466
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-03-22 11:08:56 -0600

    Update

--- modulos/movimiento/modelo.php
+++ modulos/movimiento/modelo.php
@@ -205,7 +205,7 @@
 			
 			if(!isset($option["select"]))	$option["select"]	=array();
 
-#/*			
+/*			
 			$option["select"][]	="m1.*";
 			$option["select"]["CASE WHEN SUM(m1.orden)>0 THEN SUM(m1.orden)	END"]	="orden";
 			$option["select"]["CASE WHEN SUM(m1.pago)>0 THEN SUM(m1.pago) END"]		="pago";
@@ -229,7 +229,7 @@
 			$option["select"]["IF(SUM(m1.orden)-SUM(m1.pago)!=0 AND COMPRA=1, '#ff0000','')"]="color1";
 			$option["select"]["IF(SUM(m1.orden)-SUM(m1.pago)!=0 AND VENTA=1, '#1bce54','')"]="color2";    
 			$option["select"]["IF(SUM(m1.orden)-SUM(m1.pago)=0, '#ccc','')"]="color3";
-			
+			*/
 			$option["from"]		="
 				(
 					SELECT  

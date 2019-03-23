b9373de2b7ea41268de690602edbe80b18ef2bba
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-03-22 19:00:08 -0600

    Update

--- nucleo/sesion.php
+++ nucleo/sesion.php
@@ -34,10 +34,13 @@
 			require_once($pre_path	."nucleo/general.php");													
 		}
 		if(!class_exists($sys_class) AND @file_exists($pre_path	."modulos/{$sys_class}/modelo.php"))		
+		{
 			require_once($pre_path	."modulos/{$sys_class}/modelo.php");
-						
+		}				
 		$pre_path.="../";
 	}
+	
+	
 	
 	/*
 	for($a=0; $a<10; $a++)

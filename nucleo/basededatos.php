18cd7fb9c5e520738f13626160229ee2ca25e9fd
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-03-27 15:51:59 -0600

    Update

--- nucleo/basededatos.php
+++ nucleo/basededatos.php
@@ -2,7 +2,7 @@
 	
 	class basededatos 
 	{    
-		var $OPHP_database=array(
+		private var $OPHP_database=array(
 			"user"		=>"admin_evigra",
 			"pass"		=>"EvG30JiC06",
 			#"name"		=>"admin_server",			

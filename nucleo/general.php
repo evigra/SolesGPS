9d206e5c3d140e479c626527bd8900d17c8b7ee4
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-05-20 17:55:55 -0500

    Update

--- nucleo/general.php
+++ nucleo/general.php
@@ -485,6 +485,7 @@
 		##############################################################################		 		
 		public function __SAVE($datas=NULL,$option=NULL)
     	{
+    		$this->__PRINT_R($datas);
     		if(!isset($this->sys_private["field"]) OR $this->sys_private["field"]=="")
 	    		$this->__FIND_FIELD_ID();
     	

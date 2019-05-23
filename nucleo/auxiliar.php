cfec16519ad2f2423b41628c873d6b7b0ebc379d
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-05-22 23:52:39 -0500

    Update

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -1436,7 +1436,7 @@
 						\"class_one\"		=>\"$class_one\",
 					);
 				
-					#@$"."this->sys_fields[\"$campo\"][\"obj\"]		=new {$valor["class_name"]}($"."option_$campo);
+					@$"."this->sys_fields[\"$campo\"][\"obj\"]		=new {$valor["class_name"]}($"."option_$campo);
 
 					if(isset($"."json))
 					{								

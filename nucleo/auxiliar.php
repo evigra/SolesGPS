d6efb2636842b19b6e73a1d26a24612037f16a49
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-05-22 20:30:32 -0500

    Update

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -1565,6 +1565,67 @@
 		}
 		*/
     	##############################################################################    
+		public function __MANY2MANY($option)		
+		{
+			
+			$class_id			=@$option["class_id"];
+			$class_one			=$option["class_one"];
+			$class_one_id		=$option["class_one_id"];
+			
+			$campo				=$option["class_field"];
+			$class_field_id		=$option["class_field_id"];
+			$valor				=$option["class_field_value"];
+			
+			$words				=$option["words"];                                                                                                                                                                                                                                                          
+			$index				=$option["view"];
+/*
+			if($this->sys_recursive<3)
+			{
+				if(isset($option["json"]))
+				{
+					$json	=$option["json"];										
+				}
+							
+				$eval="		
+					if(isset($"."json))
+					{								
+						$"."sys_primary_field								=$"."this->sys_fields[\"$campo\"][\"obj\"]->sys_private["field"];
+				
+						if(isset($"."class_id) AND $"."class_id>0)
+							$"."json[\"row\"][\"$"."sys_primary_field\"]	=$"."class_id;
+						
+						$"."this->sys_fields[\"$campo\"][\"obj\"]->__SAVE($"."json);
+					}
+					
+					$"."view   												=$"."this->__TEMPLATE(\"sitio_web/html/" . $valor["class_template"]. "\");													
+					
+					$"."this->sys_fields[\"$campo\"][\"obj\"]->words[\"many2one_form\"]		=$"."this->sys_fields[\"$campo\"][\"obj\"]->__VIEW_CREATE($"."this->sys_fields[\"$campo\"][\"obj\"]->sys_var[\"module_path\"] . \"html/create\");	
+					$"."this->sys_fields[\"$campo\"][\"obj\"]->words							=$"."this->sys_fields[\"$campo\"][\"obj\"]->__INPUT($"."this->sys_fields[\"$campo\"][\"obj\"]->words,$"."this->sys_fields[\"$campo\"][\"obj\"]->sys_fields);    
+													
+					$"."this->sys_fields[\"$campo\"][\"obj\"]->words[\"many2one_report_id\"]	=$"."campo;
+									
+					if(isset($"."words[\"html_head_js\"]) AND isset($"."this->sys_fields[\"$campo\"][\"obj\"]->words[\"html_head_js\"]))								
+						$"."words[\"html_head_js\"] 						.= $"."this->sys_fields[\"$campo\"][\"obj\"]->words[\"html_head_js\"];
+									
+					$"."option_report										=array();				
+													
+					$"."option_report[\"template_title\"]	                = $"."this->sys_fields[\"$campo\"][\"obj\"]->sys_var[\"module_path\"] . \"html/report_title\";
+					$"."option_report[\"template_body\"]	                = $"."this->sys_fields[\"$campo\"][\"obj\"]->sys_var[\"module_path\"] . \"html/report_body\";
+					$"."option_report[\"template_create\"]	                = $"."this->sys_fields[\"$campo\"][\"obj\"]->sys_var[\"module_path\"] . \"html/create\";
+					$"."option_report[\"template_option\"]	                = $"."option;
+					
+					$"."option_report[\"name\"]	                			= '$campo';
+					
+					$"."report_procedure									=$"."this->sys_fields[\"$campo\"][\"obj\"]->__VIEW_REPORT($"."option_report);
+					$"."this->sys_fields[\"$campo\"][\"obj\"]->words[\"many2one_report\"]		=$"."report_procedure[$"."index];				
+
+					$"."words[\"$campo\"]  									=$"."this->__REPLACE($"."view,$"."this->sys_fields[\"$campo\"][\"obj\"]->words);									
+				";				
+				eval($eval);	
+			}			
+*/
+			return $words;
+		}
 
 		public function __REPORT_MANY2ONE_JS($data)
 		{

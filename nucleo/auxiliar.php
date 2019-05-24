f105893459b49e926d54bfac66a0537059668868
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-05-24 17:47:29 -0500

    Update

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -671,17 +671,17 @@
 								);													
 								$"."this->sys_fields[\"$campo\"][\"obj\"]   =new {$valor["class_name"]}($"."option"."_obj_$campo);
 								
-								$"."sys_table			=$"."this->sys_fields[\"$campo\"][\"obj\"]->sys_table;
+								#$"."sys_table			=$"."this->sys_fields[\"$campo\"][\"obj\"]->sys_table;
 							";		
 							eval($eval);					
-
+/*
 							$comando_sql 			="CREATE TABLE IF NOT EXISTS temp_{$sys_table} LIKE {$sys_table};";						
 							$this->__EXECUTE($comando_sql);
 
 							$comando_sql 			="select * FROM temp_{$sys_table};";
 
 							$this->__PRINT_R($this->__EXECUTE($comando_sql));
-
+*/
 						}	
 
 

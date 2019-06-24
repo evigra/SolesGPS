a565a042234c901da6f21707acb2c2a1478ec4b0
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-06-24 11:52:13 -0500

    Update

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -346,6 +346,7 @@
 				    if(isset($_SESSION["company"]["razonSocial"]) AND isset($_SESSION["user"]["name"]))
 				    {
 					    $words["sys_empresa"]        	=$_SESSION["company"]["nombre"];
+					    $words["system_company"]       	=$_SESSION["company"]["nombre"];
 					    $words["system_user"]           =$_SESSION["user"]["name"];
 					    $words["system_logo"]           =$this->__SHOW_FILE($_SESSION["company"]["files_id"]);
 					    $words["system_img"]           	=$this->__HTML_USER();

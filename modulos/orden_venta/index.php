0be1ba5db0a0fa0326c37634831ffc15818608dd
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-06-27 18:59:20 -0500

    Update

--- modulos/orden_venta/index.php
+++ modulos/orden_venta/index.php
@@ -121,7 +121,7 @@
 	$objeto->words["module_title"]              =$module_title . "Orden de Venta";
 	
 	$objeto->words["module_left"]           =$objeto->__BUTTON($module_left);
-	$objeto->words["module_center"]         =$objeto->__BUTTON($module_center);
+	$objeto->words["module_center"]         ="{flow}";
 	$objeto->words["module_right"]          =$objeto->__BUTTON($module_right);    
     
 	$objeto->words["html_head_title"]		=	"SOLES GPS :: {$_SESSION["company"]["razonSocial"]} :: {$objeto->words["module_title"]}";

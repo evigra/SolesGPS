7a4e415662c3a01daafdfa6a14d15eeed2f19e0a
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-07-01 13:50:37 -0500

    Update

---
+++ modulos/print_pdf/modelo.php
@@ -0,0 +1,41 @@
+<?php
+	class print_pdf extends general
+	{   
+		##############################################################################	
+		##  Propiedades	
+		##############################################################################
+		var $sys_filter		=array(
+				"name"=>"d.name",		
+		);
+		var $sys_fields		=array(
+			"id"	    =>array(
+			    "title"             => "id",
+			    "type"              => "primary key",
+			),		
+			"module"	    	=>array(
+			    "title"             => "Modulo",
+			    "type"              => "input",
+			),
+			"section"	    	=>array(
+			    "title"             => "Seccion",
+			    "type"              => "input",
+			),
+		);				
+		##############################################################################	
+		##  Metodos	
+		##############################################################################
+		public function __CONSTRUCT($option=NULL)
+		{
+			parent::__CONSTRUCT($option);
+
+			$option=array(
+				"id"		=>$this->sys_fields["module"]["id"],
+				"section"	=>$this->sys_fields["module"]["section"],
+				"module"	=>$this->sys_fields["module"]["module"],									
+			);			
+			$this->PDF_PRINT($option)
+		}
+	  				
+		
+	}
+?>

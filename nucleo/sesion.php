54fd781d165ac7ff7a903bb470699f8ac70b58a3
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-07-04 22:18:04 -0500

    Update

--- nucleo/sesion.php
+++ nucleo/sesion.php
@@ -83,13 +83,15 @@
 			$modulos 				=$objeto->__EXECUTE($comando_sql);    
 			
 			
-	
+			$objeto->__PDF();
+			/*
 			foreach($modulos as $modulo)
 			{
 				if(file_exists($pre_path	."modulos/{$modulo["clase"]}/modelo.php")) 				
 					require_once($pre_path	."modulos/{$modulo["clase"]}/modelo.php");	
 			}
-			$objeto->__PDF();
+			*/
+			
 			if(@$_REQUEST["setting_company"]>0)
 			{
 				$comando_sql="

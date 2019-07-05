e11356929b27bcadf4f884644634c39462afd0cd
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-07-04 21:47:22 -0500

    Update

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -1065,10 +1065,10 @@
 			$pdf->writeHTML($html, true, 0, true, 0);
 			$pdf->lastPage();
 
-			#echo $html;
+			echo $html;
 			if(!isset($_SESSION["pdf"]["save_name"]))	$_SESSION["pdf"]["save_name"]=$_SESSION["pdf"]["title"];
 
-			$pdf->Output($_SESSION["pdf"]["save_name"], 'I');
+			#$pdf->Output($_SESSION["pdf"]["save_name"], 'I');
 			exit;
 		}		
     	##############################################################################    

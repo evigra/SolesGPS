c856391c9592d7b524046420352d01a5d28de3ce
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-07-04 22:19:13 -0500

    Update

--- nucleo/sesion.php
+++ nucleo/sesion.php
@@ -79,18 +79,19 @@
 			
 			
 			
-			$comando_sql			="SELECT * FROM modulos ";		
+			$comando_sql			="SELECT * FROM modulos ";
+			$comando_sql			="SELECT * FROM modulos LIMIT 1,5";				
 			$modulos 				=$objeto->__EXECUTE($comando_sql);    
 			
 			
 			$objeto->__PDF();
-			/*
+			#/*
 			foreach($modulos as $modulo)
 			{
 				if(file_exists($pre_path	."modulos/{$modulo["clase"]}/modelo.php")) 				
 					require_once($pre_path	."modulos/{$modulo["clase"]}/modelo.php");	
 			}
-			*/
+			#*/
 			
 			if(@$_REQUEST["setting_company"]>0)
 			{

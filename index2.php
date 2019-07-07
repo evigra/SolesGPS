9b94b2385a0d743b3cf0d305dbaf89d5ca5c5111
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-07-07 09:03:44 -0500

    Update

--- index2.php
+++ index2.php
@@ -1,4 +1,5 @@
 <?php
+	ini_set('display_errors', 1);				
 	
 	include('nucleo/cer/Crypt/RSA.php');
 	include('nucleo/cer/File/X509.php');
@@ -18,14 +19,15 @@
 
 
 
-	$data_file = file_get_contents('VIGE850830GKA.key.pem');	
+	$data_file = file_get_contents('VIGE850830GKA.key');	
 	
 	$objeto = new Crypt_RSA();	
 	$cert = $objeto->loadKey($data_file);
 	
 	#/*
 	echo "<pre>";
-	#print_R($objeto);	
+	print_R($data_file);	
+	#print_R($objeto);
 	print_R($cert);
 	echo "</pre>";
 	#*/

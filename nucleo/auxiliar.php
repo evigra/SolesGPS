adec762d9db1984f88a7d92c7e21cdee107f50af
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-07-06 21:18:59 -0500

    Update

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -1993,7 +1993,7 @@
                     	if(!isset($option["color"]))				$option["color"]=array();                    	
                     	#if(!isset($option["color"]["black"]))		$option["color"]["black"]="1==1";
                     	#if(!isset($option["color"]["red"]))		
-                    	$option["color"]["red"]="1==1";
+                    	$option["color"]["red"]="$"."row=['estatus']=1";
                                         	
                     	foreach($option["color"] as $color => $filter)
                     	{							

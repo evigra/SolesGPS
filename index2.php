30156f74ab9702e58266e35d05a4de8603eb4c9c
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-07-07 08:40:33 -0500

    Update

--- index2.php
+++ index2.php
@@ -1,23 +1,32 @@
 <?php
 	
 	include('nucleo/cer/File/X509.php');
-
-	$x509 = new File_X509();	
-	$cer=$_GET["cer"];
-	
-	
-	$pemca = file_get_contents('VIGE850830GKA.cer');	
-	$cert = $x509->loadX509($pemca);
-
-
+	include('nucleo/cer/File/X509.php');
 	
 
+	$data_file = file_get_contents('VIGE850830GKA.cer');	
+	
+	$objeto = new File_X509();	
+	$cert = $objeto->loadX509($data_file);
+	
+	/*
 	echo "<pre>";
 	print_R($cert);	
 	echo "</pre>";
+	#*/
 
 
 
 
+	$data_file = file_get_contents('VIGE850830GKA.key');	
+	
+	$objeto = new Crypt_RSA();	
+	$cert = $objeto->loadKey($data_file);
+	
+	#/*
+	echo "<pre>";
+	print_R($cert);	
+	echo "</pre>";
+	*/
 
 ?>

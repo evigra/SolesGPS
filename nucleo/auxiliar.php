f68ad0aa8ad457dfa220b950bb1b2a6b46cf79a3
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-07-08 08:34:17 -0500

    Update

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -1463,14 +1463,14 @@
 					    			#$this->__PRINT_R($this->sys_fields[$campo]["obj"]->sys_fields[$row_field]);
 					    			$label	=$this->sys_fields["$campo"]["values"][0][$this->sys_fields["$campo"]["class_field_l"]];					    			
 					    			
-					    			/*
-									foreach($this->sys_fields[$campo]["values"][0] as $row_field=>$row_value)
+					    			#/*
+									foreach($this->sys_fields[$campo]["obj"]->sys_fields as $row_field=>$row_value)
 									{
 										$titulo_aux=@$this->sys_fields[$campo]["obj"]->sys_fields[$row_field]["title"];										
-										$words[$campo.".$row_field"]  =@$row_value . @$valor["br"] . @$titulo_aux;
+										$words[$campo.".$row_field"]  =@$this->sys_fields["$campo"]["values"][0][$row_field] . @$valor["br"] . @$titulo_aux;
 										#$words[$campo."$row_field"]  =$row_value. @$valor["br"] . $titulo;																																		
 									}
-									*/
+									#*/
 					    			
 					    		}	
 					    	}

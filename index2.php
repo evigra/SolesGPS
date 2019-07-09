4ac995a2073abf1b09f5b62fc12c0f33b9c870eb
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-07-09 12:49:13 -0500

    Update

--- index2.php
+++ index2.php
@@ -13,43 +13,41 @@
 	
 	
 	echo "<pre>";
-	#print_R($cert);	
+	###print_R($cert);	
 
-	#print_r($objeto->getDNProp('CN'));
-	#print_r($objeto->getDN());
-	print_r($objeto->getDN(true));
+	###print_r($objeto->getDNProp('CN'));
+	###print_r($objeto->getDN());
+	#print_r($objeto->getDN(true));
 	#print_r($objeto->getIssuerDNProp('CN'));
 	#print_r($objeto->getIssuerDN());
-	print_r($objeto->getDNProp(true));
-	print_r($objeto->getIssuerDN(true));
+	#print_r($objeto->getDNProp(true));
+	#print_r($objeto->getIssuerDN(true));
+	echo "</pre>";	
+	#echo $objeto->getPublicKey();
+
+#*/
+
+	$privatekey = file_get_contents('VIGE850830GKA.key');	
 	
+	$privKey = new Crypt_RSA();
+	#extract($privKey->createKey());
+	$privKey->loadKey($privatekey);
 
+	#$x509 = new File_X509();
+	$objeto->setPrivateKey($privKey);
+	#$x509->setDNProp('id-at-organizationName', 'phpseclib demo cert');
+
+	#$csr = $x509->signCSR();
+
+	#echo $x509->saveCSR($csr);
 	
-
-
-
-
-
-	echo "</pre>";
 	
-	echo $objeto->getPublicKey();
-
-
-
-
-	$data_file = file_get_contents('VIGE850830GKA.key');	
-	
-	$objeto = new Crypt_RSA();	
-	$cert = $objeto->loadKey($data_file);
-
-	
-
-	#/*
 	echo "<pre>";
-	#print_R($data_file);	
-	#print_R($objeto);
-	#print_R($cert);
-	echo "</pre>";
-	#*/
+	print_R($cert);	
 
+	#print_r($objeto->getDN(true));
+	#print_r($objeto->getDNProp(true));
+	#print_r($objeto->getIssuerDN(true));
+	echo "</pre>";	
+	
 ?>

5e4aa85f4bc4e169b62d6acd0f643f5b75a0f4db
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-07-13 11:55:59 -0500

    Update

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -1401,7 +1401,7 @@
 										$selected="ui-state-default";
 										if($valor["value"]==$value) 										
 											$selected="ui-state-hover";
-										$options.="<td class=\"$selected\" style=\"padding-left:7px; padding-right:10px;\"><a date=\"$value\">$text</a></td>";			            
+										$options.="<td style=\"padding-left:7px; padding-right:10px;\"><a  class=\"$selected\" date=\"$value\">$text</a></td>";			            
 							    	}
 							    }
 								if(@$this->sys_private["section"]=="show")

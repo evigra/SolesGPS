2292d8f78b4764eff5d2177c29266b15f8dd8d74
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-07-20 19:02:37 -0500

    Update

--- nucleo/general.php
+++ nucleo/general.php
@@ -44,7 +44,7 @@
 			
 			#ini_set('display_errors', 1);				
 			
-			$this->__PRINT_R($_SERVER);
+			#$this->__PRINT_R($_SERVER["SERVER_NAME"]);
 			
 			if(isset($_SESSION["var"]["server_error"]) AND in_array(@$_SERVER["SERVER_NAME"],@$_SESSION["var"]["server_error"]))
 			{	

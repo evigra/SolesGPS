b39c6150a9141cc617d0422e4eae62ffb2231dbd
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-07-31 18:04:39 -0500

    Update

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -239,7 +239,7 @@
 			$sesion 			=array("apikey"=>"NJQ6UF3POVNMC00SFEWL");
 			$sesion 			=array("apikey"=>"SQ9CVE4AD6MVJ8BXRON6");
 			
-			$url 				="https://panel.capiwha.com/send_message.php";
+			$url 				="https://panel.apiwha.com/send_message.php";
 			$vars 				=$sesion;				
 			$vars["number"]		=$data["telefono"];
 						

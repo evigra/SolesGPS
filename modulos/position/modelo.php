ca305aa27fb9b6bff9b1af532f4d14b6dca7d7d0
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-08-05 12:41:26 -0500

    Update

--- modulos/position/modelo.php
+++ modulos/position/modelo.php
@@ -162,6 +162,43 @@
 									
 		}
 		##############################################################################
+		public function cron_recordatorio_ALERTAS()
+    	{			    		
+			$comando_sql="
+				SELECT v.*, md5(ID) as md5_id 
+				FROM V_ULTIMOREPORTE v 
+				WHERE 1=1
+					AND tipo_vehiculo='GPS'
+					AND reporto_hace>'23:30:00'
+			";
+			$position_data 		=$this->__EXECUTE($comando_sql);
+			
+			#$this->__PRINT_R($position_data);
+			if(count($position_data)>0)
+			{								
+				foreach($position_data as $row)
+				{					
+					$mensaje= "SolesGPS :: Detectada ausencia de senal de {$row["NOMBRE"]}, Tiempo ausente {$row["REPORTO_HACE"]}";
+					$row["TEL_COMPANY"]="5213141182618";
+										
+					$this->__WA(
+						array(
+							"telefono"=>$row["TEL_COMPANY"], 
+							"mensaje"=>"[{$row["NOMBRE"]}] :: {$row["REPORTO_HACE"]}\nRecordatorio de ausencia de senal detectada. \n
+Puede apoyarse con el siguiente link
+					
+http://solesgps.com/seguimientos/&a={$row["md5_id"]}
+						
+								Sistema Automatico SolesGPS"
+						)
+					);					
+					
+					#$this->__SMS("+{$row["TEL_COMPANY"]}", $mensaje, false, "");					
+					#$this->__WA(array("telefono"=>$row["TEL_COMPANY"], "mensaje"=>$mensaje));
+				}
+			}
+		}
+
 		public function cron_retraso_ALERTAS()
     	{			    		
 			$comando_sql="
@@ -196,32 +233,6 @@ http://solesgps.com/seguimientos/&a={$row["md5_id"]}
 					
 					#$this->__SMS("+{$row["TEL_COMPANY"]}", $mensaje, false, "");					
 					#$this->__WA(array("telefono"=>$row["TEL_COMPANY"], "mensaje"=>$mensaje));
-				}
-			}
-		}
-		##############################################################################
-		public function cron_retraso_WA()
-    	{			    		
-			$comando_sql="
-				SELECT v.*, md5(ID) as md5_id 
-				FROM V_ULTIMOREPORTE v 
-				WHERE 1=1
-					AND tipo_vehiculo='GPS'
-					AND reporto_hace>'00:30:00'
-					AND reporto_hace<'01:10:00'
-					
-			";
-			$position_data 		=$this->__EXECUTE($comando_sql);
-			
-			if(count($position_data)>0)
-			{								
-				foreach($position_data as $row)
-				{					
-					$mensaje= "SolesGPS :: Detectada ausencia de senal de {$row["NOMBRE"]}, Tiempo ausente {$row["REPORTO_HACE"]}";
-					#$row["TEL_COMPANY"]="5213143520972";
-					
-					$this->__SMS("+{$row["TEL_COMPANY"]}", $mensaje, false, "");
-					$this->__WA(array("telefono"=>$row["TEL_COMPANY"], "mensaje"=>$mensaje));
 				}
 			}
 		}

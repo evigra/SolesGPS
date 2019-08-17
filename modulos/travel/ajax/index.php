8e8113b3abf3d16fe39922c099dc2a719c000707
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-08-17 15:26:47 -0500

    Update

--- modulos/travel/ajax/index.php
+++ modulos/travel/ajax/index.php
@@ -3,7 +3,6 @@
 	$option				=array();	
 
 	$item_obj				=new item($option);	
-
 	
 	$objeto				=new travels($option);	
 	$travels_datas		=$objeto->__VIAJE_HOY();
@@ -20,23 +19,14 @@
 			$item_datas=$item_obj->__BROWSE($items["item_id"]);
 			foreach($item_datas["data"] as $travel)
 			{
-				$puntos_array = explode("|", $travel["long1"]);
-				$total=count($puntos_array);
+				$puntos_array 					= explode("|", $travel["long1"]);
+				$total							=count($puntos_array);
 				foreach($puntos_array as $index=>$punto)
 				{				
-					$coord = explode(",",$punto);
-					if($index==0)
-					{					
-						$js.="origen=LatLng({latitude:{$coord[1]},longitude:{$coord[0]}});";
-					}
-					else if($index==$total-2)
-					{
-						$js.="destino=LatLng({latitude:{$coord[1]},longitude:{$coord[0]}});";
-					}
-					else if($index==$total-1)
-					{
-						$js.="tracert(origen,destino,waypts);";
-					}
+					$coord 						= explode(",",$punto);
+					if($index==0)				$js.="origen=LatLng({latitude:{$coord[1]},longitude:{$coord[0]}});";
+					else if($index==$total-2)	$js.="destino=LatLng({latitude:{$coord[1]},longitude:{$coord[0]}});";
+					else if($index==$total-1)	$js.="tracert(origen,destino,waypts);";
 					else
 					{
 						$js.="

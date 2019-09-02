46a9a7198d7a9506020cdc2e002fd3704aceadbe
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-09-02 00:25:17 -0500

    Update

--- modulos/position/index.php
+++ modulos/position/index.php
@@ -13,71 +13,6 @@
 	$files_js=array("maps","responsivevoice");
 	$files_js=array("maps");
 
-/*
-	if($objeto->sys_section=="report")
-	{
-		$option=array();
-        $files_js=array("../{$objeto->sys_module}js/index");
-		$option["template_title"]	                = $objeto->sys_module . "html/report_title";
-		$option["template_body"]	                = $objeto->sys_module . "html/report_body";
-		#$option["template_create"]	                = $objeto->sys_module . "html/report_form";
-		$option["template_search"]	                = $objeto->sys_module . "html/report_form";
-		
-		$data										=$objeto->time_position($option);
-		$objeto->words["module_body"]				=$data["html"];
-    }
-	elseif($objeto->sys_section=="report_distance")
-	{
-		$option=array();
-        $files_js=array("../{$objeto->sys_module}js/index");
-		$option["template_title"]	                = $objeto->sys_module . "html/report_distance_title";
-		$option["template_body"]	                = $objeto->sys_module . "html/report_distance_body";
-		
-		$data										=$objeto->distancias($option);
-		$objeto->words["module_body"]				=$data["html"];
-    }
-
-	elseif($objeto->sys_section=="show")
-	{	
-	    $files_js=array("../{$objeto->sys_module}js/index");
-    	$objeto->words["module_body"]               =$objeto->__VIEW_CREATE($objeto->sys_module . "html/show");	
-    	$objeto->words                              =$objeto->__INPUT($objeto->words,$objeto->sys_fields);      
-    }    
-    elseif($objeto->sys_section=="historyMap") // $objeto->sys_section=map
-    {
-		$objeto->words["system_module"]             =$objeto->__TEMPLATE($objeto->sys_html."system_module_not");
-		$files_js[]="../{$objeto->sys_module}js/history";
-    
-    	$objeto->words["module_body"]               =$objeto->__VIEW_CREATE($objeto->sys_module . "html/map");	
-    	$objeto->words                              =$objeto->__INPUT($objeto->words,$objeto->sys_fields);      
-    }
-    elseif($objeto->sys_section=="historyStreet") // $objeto->sys_section=map
-    {
-		$objeto->words["system_module"]             =$objeto->__TEMPLATE($objeto->sys_html."system_module_not");
-		$files_js[]="../{$objeto->sys_module}js/history";
-    
-    	$objeto->words["module_body"]               =$objeto->__VIEW_CREATE($objeto->sys_module . "html/streetmap");	
-    	$objeto->words                              =$objeto->__INPUT($objeto->words,$objeto->sys_fields);      
-    }
-    elseif($objeto->sys_section=="streetmap") // $objeto->sys_section=map
-    {
-    	
-		$objeto->words["system_module"]             =$objeto->__TEMPLATE($objeto->sys_html."system_module_not");
-		$files_js[]="../{$objeto->sys_module}js/map";
-    
-    	$objeto->words["module_body"]               =$objeto->__VIEW_CREATE($objeto->sys_module . "html/streetmap");	
-    	$objeto->words                              =$objeto->__INPUT($objeto->words,$objeto->sys_fields);      
-    	//
-    }
-     else
-     */
-		$module_right=array(
-		    array("create"=>"Crear"),
-		    array("graph"=>"Grafica"),
-		    #array("write"=>"Modificar"),
-		    #array("kanban"=>"Kanban"),
-		    array("report"=>"Reporte"),
-		);
 
 
 	#if($objeto->sys_private["section"]=="graph")
@@ -86,8 +21,6 @@
 		$module_right=array(
 		    array("create"=>"Crear"),
 		    array("graph"=>"Grafica"),
-		    #array("write"=>"Modificar"),
-		    #array("kanban"=>"Kanban"),
 		    array("report"=>"Reporte"),
 		);
 	
@@ -105,46 +38,12 @@
 		$data										=$objeto->__VIEW_GRAPH($option);		
 		$objeto->words["module_body"]				=$data;
     }    
-    /*
-    else // $objeto->sys_section=map
-    {
-		$objeto->words["system_module"]             =$objeto->__TEMPLATE($objeto->sys_html."system_module_not");
-		$files_js[]="../{$objeto->sys_module}js/map";
-    
-    	$objeto->words["module_body"]               =$objeto->__VIEW_CREATE($objeto->sys_module . "html/map");	
-    	$objeto->words                              =$objeto->__INPUT($objeto->words,$objeto->sys_fields);      
-    	//
-    }
-    */
-    
-    $module_left=array(
-        array(
-        	"id"	=>"year",
-        	"title"	=>"AÑO",
-        	"group"	=>"",
-        ),
-        array(
-        	"id"	=>"month",
-        	"title"	=>"MES",
-        	"group"	=>"",
-        ),
 
-        array(
-        	"id"	=>"day",
-        	"title"	=>"DIA",
-        	"group"	=>"",
-        ),
-    );
-        
 	$objeto->words["html_head_js"]              =$objeto->__FILE_JS($files_js);								# ARCHIVOS JS DEL MODULO
-	$objeto->words["html_head_css"]              =$objeto->__FILE_CSS(array("../sitio_web/css/basicItems"));
+#	$objeto->words["html_head_css"]              =$objeto->__FILE_CSS(array("../sitio_web/css/basicItems"));
 	
-	#$files_css=array();
-	#$files_css[]="../{$objeto->sys_module}css/map1";
-	#$files_css[]="../{$objeto->sys_module}css/map2";
-	#$objeto->words["html_head_css"]             =$objeto->__FILE_CSS($files_css);								# ARCHIVOS CSS DEL MODULO
 
-    $objeto->words["system_submenu2"]           =$objeto->menu_vehicle();    	
+#    $objeto->words["system_submenu2"]           =$objeto->menu_vehicle();    	
 
 	$objeto->words["module_title"]              ="REPORTE DE POSICIONES";
 	$objeto->words["module_left"]               =""; #$objeto->__CHECK($module_left,"DATE");

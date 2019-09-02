9916f62193fd6282689df1873ada5924acdb8254
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-09-02 02:01:18 -0500

    Update

--- modulos/position/index.php
+++ modulos/position/index.php
@@ -33,8 +33,8 @@
 		$option["select"]["left(right(p.devicetime,8),5)"]	="devicetime";
 		$option["select"][]									="p.speed";
 
-		#$option["where"][]									="left(now(),10)=left(p.devicetime,10)";
-		$option["where"][]									="left('2019-08-31',10)=left(p.devicetime,10)";
+		#$option["where"][]									="left(now(),10)=left(DATE_SUB(p.devicetime,INTERVAL {$_SESSION["user"]["huso_h"]} HOUR),10)";
+		$option["where"][]									="left('2019-08-31',10)=left(DATE_SUB(devicetime,INTERVAL {$_SESSION["user"]["huso_h"]} HOUR),10)";
 		$option["order"]									="devicetime ASC";
 		#$option["limit"]									="1000";
 

003bd8c5d6f54918938f3c36943ddf02c181db13
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-09-02 01:26:35 -0500

    Update

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -2419,7 +2419,13 @@
 					<script>
 							function drawChart() 
 							{
-								var data = google.visualization.arrayToDataTable([$fila]);
+								var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2013',  1000,      400],
+          ['2014',  1170,      460],
+          ['2015',  660,       1120],
+          ['2016',  1030,      540]
+        ]);
 
 								var options = {
 									title: 'Company Performance',
@@ -3298,7 +3304,6 @@
 							google.charts.setOnLoadCallback(drawChart);
 						</script>			
 				    ";    		    
-
 				}		
 			}
 			return $return;

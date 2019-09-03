888ca595823e7a589900b175d9b697c16c1e7ca6
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-09-02 19:36:45 -0500

    Update

--- nucleo/auxiliar.php
+++ nucleo/auxiliar.php
@@ -3296,30 +3296,30 @@
 				    			$grafica=$valor["graph"];
 				    	}
 				    	else
-				    	{
+				    	
 				    		$datos=$valor;
 				    
-							$return.="
-								<script type='text/javascript'>
-									google.charts.load('current', {'packages':['corechart']});
-									google.charts.setOnLoadCallback(drawChart);
-									
-									function drawChart() 
-									{
-										var data = google.visualization.arrayToDataTable([$datos]);
+						$return.="
+							<script type='text/javascript'>
+								google.charts.load('current', {'packages':['corechart']});
+								google.charts.setOnLoadCallback(drawChart);
+								
+								function drawChart() 
+								{
+									var data = google.visualization.arrayToDataTable([$datos]);
 
-										var options = {
-											title: 'Company Performance',
-											hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
-											vAxis: {minValue: 0}
-										};
+									var options = {
+										title: 'Company Performance',
+										hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
+										vAxis: {minValue: 0}
+									};
 
-										var chart = new google.visualization.".$grafica."(document.getElementById('chart_div'));
-										chart.draw(data, options);
-									}									
-								</script>			
-							";
-						}	
+									var chart = new google.visualization.".$grafica."(document.getElementById('chart_div'));
+									chart.draw(data, options);
+								}									
+							</script>			
+						";
+							
 				    }    		    
 				}		
 			}

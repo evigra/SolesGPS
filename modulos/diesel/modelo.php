568bc14e8092921550b027283146052e4349cdef
Eduardo Vizcaino <evigra@hotmail.com>
2020-02-12 23:18:49 -0600

    Update

--- modulos/diesel/modelo.php
+++ modulos/diesel/modelo.php
@@ -29,15 +29,21 @@
 
 		public function __VIEW_GRAPH($option_graph=array(),$template=NULL)
 		{
+		    
+		
 			$option				=array();	
-			$option["select"]["left(right(DATE_SUB(devicetime,INTERVAL {$_SESSION["user"]["huso_h"]} HOUR),8),5)"]	="devicetime";
-			$option["select"][]				="left(extract_JSON(p.attributes,'io3'),4)";
+			#$option["select"]["left(right(DATE_SUB(devicetime,INTERVAL {$_SESSION["user"]["huso_h"]} HOUR),8),5)"]	="devicetime";
+			$option["select"]["right(left(devicetime,16),11)"]	="devicetime";
+			$option["select"][]				="if(extract_JSON(p.attributes,'io3') is null ,0,round(avg(left(extract_JSON(p.attributes,'io3'),4)),2))";
 			$option["where"][]				="DATE_SUB('{$this->sys_fields["start"]["value"]}',INTERVAL {$_SESSION["user"]["huso_h"]} HOUR)<=DATE_SUB(devicetime,INTERVAL {$_SESSION["user"]["huso_h"]} HOUR)";
 			$option["where"][]				="DATE_SUB('{$this->sys_fields["end"]["value"]}',INTERVAL {$_SESSION["user"]["huso_h"]} HOUR)>=DATE_SUB(devicetime,INTERVAL {$_SESSION["user"]["huso_h"]} HOUR)";
 			$option["where"][]				="deviceid ='{$this->sys_fields["deviceid"]["value"]}'";							
+            $option["group"]	            ="left(devicetime,15)";
 			$option["order"]				="devicetime ASC";
+			
+			
 			$option["title"]	="['Hora','% Tanque de Diesel'],";
-			$option["echo"]	="DIESEL";
+			#$option["echo"]	="DIESEL";
 			$option_graph["AreaChart"]=$option;
 
 			return parent::__VIEW_GRAPH($option_graph);

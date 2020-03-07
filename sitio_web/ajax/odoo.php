78201cc096a900a74e6eda7fe96faf76d4a5162a
Eduardo Vizcaino <evigra@hotmail.com>
2020-03-06 19:11:10 -0600

    Update

--- sitio_web/ajax/odoo.php
+++ sitio_web/ajax/odoo.php
@@ -8,23 +8,15 @@
 	$option["where"]    =array();
 	#$option["where"][]  ="leido=0";
 	$option["where"][]  ="odoo!=1";
-	$option["order"]    ="devicetime";
-	$option["echo"]     ="ODOO";
+	$option["order"]    ="devicetime DESC";
+    $option["limit"]     ="1000";
+#	$option["echo"]     ="ODOO";
 	
 	$data               =$objeto->__BROWSE($option);
 	
-	
+    	
+    $objeto->__EXECUTE("UPDATE positions SET odoo=1");	
 	#$objeto->__PRINT_R($data);
-	#/*
-	foreach($data["data"] as $row)
-	{
-        $objeto->sys_private["id"]		=$row["p_id"];
-        $data_update["odoo"]		="1";
-        
-        $option_position			=array();
-        $objeto->__SAVE($data_update,$option_position);	
-	}
 		
     echo json_encode($data["data"]);
-    #*/
 ?>
